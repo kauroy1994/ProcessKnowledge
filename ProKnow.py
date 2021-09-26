546c5ca842e89e08211393ff210b24fedeec8b9c
Kaushik Roy <kaushikroy@Kaushiks-MacBook-Pro.local>
2021-09-26 11:24:04 -0400

    test commit #2

---
+++ ProKnow.py
@@ -0,0 +1,13 @@
+class SI(object):
+
+
+    def  __init__(self):
+
+        self.x = {('Q1T','Q2T'):0.0,
+                  ('Q1T','Q2F'):0.0,
+                  ('Q1F','Q2T'):0.0,
+                  ('Q1F','Q2F'):0.0}
+
+        
+
+        
